794171bf707f06a2f90ad5cb65fbbd55428b103f
Łukasz Piesiewicz <wookesh@gmail.com>
2016-10-27 13:41:18 +0200

    run -y for apt

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 if ! type "clang" > /dev/null; then
-	sudo apt-get install clang
+	sudo apt -y install clang
 	echo "no clang"
 fi
 
 if ! type "pip" > /dev/null; then
-	sudo apt-get install python-pip
+	sudo apt -y install python-pip
 fi
 
 if ! type "virtualenv" > /dev/null; then
-	sudo apt-get install virtualenv
+	sudo apt -y install virtualenv
 fi
 
 virtualenv env
